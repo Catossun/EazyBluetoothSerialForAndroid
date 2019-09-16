a960ed9b40ad3148b51e254eb5f9aa7428d3d0f7
WeiKeUp <boshun2009@gmail.com>
2019-09-16 18:24:10 +0800

    First Commit

---
+++ SourceProject/bluetoothserial/src/main/java/com/chen/weikeup/mlib/bluetoothserial/BluetoothSerialAdapter.java
@@ -0,0 +1,74 @@
+package com.chen.weikeup.mlib.bluetoothserial;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 對本機藍芽進行操作。
+ *
+ * @author Weikeup
+ */
+public class BluetoothSerialAdapter {
+    private static String TAG = BluetoothSerialAdapter.class.getSimpleName();
+    private static BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+
+    private BluetoothSerialAdapter() {
+    }
+
+    /**
+     * 取得所有已配對的裝置。
+     *
+     * @return 已配對的裝置之 Set 集合。
+     */
+    public static Set<BluetoothSerialDevice> getDevices() {
+        Set<BluetoothSerialDevice> devices = new HashSet<>();
+        if (adapter != null) {
+            adapter.cancelDiscovery();
+            for (BluetoothDevice device : adapter.getBondedDevices()) {
+                try {
+                    BluetoothSerialDevice serialDevice = new BluetoothSerialDevice(device);
+                    devices.add(serialDevice);
+                } catch (IOException e) {
+                    Log.e(TAG, "Error: ", e);
+                }
+            }
+        }
+        return devices;
+    }
+
+    /**
+     * 裝置是否支援藍芽。
+     *
+     * @return 如果裝置支援藍芽，回傳 true，否則回傳 false。
+     */
+    public static boolean isSupport() {
+        return adapter != null;
+    }
+
+    /**
+     * 裝置藍芽是否開啟。
+     *
+     * @return 如果裝置已開啟藍芽，回傳 True，否則回傳 False。
+     */
+    public static boolean isEnable() {
+        return adapter != null && adapter.isEnabled();
+    }
+
+    /**
+     * 向使用者發出開啟藍芽對話框。
+     *
+     * @param activity    用來執行 startActivityForResult() 的 Activity
+     * @param requestCode 當 Activity 執行後，傳給 onActivityResult() 使用
+     */
+    public static void callRequestEnableBluetoothActivity(Activity activity, int requestCode) {
+        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        activity.startActivityForResult(intent, requestCode);
+    }
+}
---
+++ SourceProject/bluetoothserial/src/main/java/com/chen/weikeup/mlib/bluetoothserial/BluetoothSerialDevice.java
@@ -0,0 +1,126 @@
+package com.chen.weikeup.mlib.bluetoothserial;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.UUID;
+
+/**
+ * 藍芽裝置物件。
+ *
+ * @author Weikeup
+ */
+public class BluetoothSerialDevice {
+    /**
+     * 藍芽序列傳輸服務的 UUID。
+     */
+    public static String SERIAL_PORT_SERVICE_UUID = "00001101-0000-1000-8000-00805F9B34FB";
+
+    /**
+     * <p>用來傳送資料。</p>
+     * <p>使用 out.print(data); 傳送資料</p>
+     */
+    public PrintStream out;
+
+    /**
+     * <p>用來接收資料。</p>
+     * <p>使用 in.next(); 接收資料</p>
+     */
+    public Scanner in;
+
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private UUID uuid;
+
+    BluetoothSerialDevice(BluetoothDevice device) throws IOException {
+        this.device = device;
+        uuid = UUID.fromString(SERIAL_PORT_SERVICE_UUID);
+        socket = device.createRfcommSocketToServiceRecord(uuid);
+    }
+
+    /**
+     * 取得目前連線用的 UUID 物件。
+     *
+     * @return UUID 物件。
+     */
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    /**
+     * 設定連線用的 UUID 物件。
+     *
+     * @param uuid UUID 物件。
+     */
+    public void setUUID(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    /**
+     * 與藍芽裝置是否連線。
+     *
+     * @return 如果已與藍芽裝置連線，回傳 true，否則回傳 false。
+     */
+    public boolean isConnected() {
+        return socket != null && socket.isConnected();
+    }
+
+    /**
+     * 取得目前連線的 BluetoothSocket 物件。
+     *
+     * @return BluetoothSocket 物件。
+     */
+    public BluetoothSocket getSocket() {
+        return socket;
+    }
+
+    /**
+     * 取得目前藍芽的 BluetoothDevice 物件。
+     *
+     * @return BluetoothDevice 物件。
+     */
+    public BluetoothDevice getDevice() {
+        return device;
+    }
+
+    /**
+     * <p>與藍芽裝置建立連線。</p>
+     * <p>註：此方法會阻塞呼叫該方法的執行續！</p>
+     *
+     * @throws IOException
+     */
+    public void connect() throws IOException {
+        BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+        socket.connect();
+        if (isConnected()) {
+            out = new PrintStream(socket.getOutputStream());
+            in = new Scanner(socket.getInputStream());
+        }
+    }
+
+    /**
+     * 取得目前連線的 InputStream。
+     *
+     * @return InputStream 物件。
+     * @throws IOException
+     */
+    public InputStream getInputStream() throws IOException {
+        return socket.getInputStream();
+    }
+
+    /**
+     * 取得目前連線的 OutputStream。
+     *
+     * @return OutputStream 物件。
+     * @throws IOException
+     */
+    public OutputStream getOutputStream() throws IOException {
+        return socket.getOutputStream();
+    }
+}
